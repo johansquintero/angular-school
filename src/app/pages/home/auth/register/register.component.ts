0a483b3a798b46a5c9dcf6eac9ca53bd1ad0fb29
Johan Sebastian Quintero <metalium144@gmail.com>
2024-09-29 17:06:22 -0500

    Se completo el componente de register

--- src/app/pages/home/auth/register/register.component.ts
+++ src/app/pages/home/auth/register/register.component.ts
@@ -1,13 +1,16 @@
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { AppBaseComponent } from '../../../../core/util/appBaseComponent';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { crossPasswordValidator, CustomValidators, PasswordStateMatcher } from '../../../../core/util/customValidator';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../../../core/services/auth.service';
+import { AuthRequestSignUpDto } from '../../../../core/dto/auth/authRequestSignUpDto';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
 	selector: 'app-register',
@@ -19,6 +22,9 @@ import { MatButtonModule } from '@angular/material/button';
 export class RegisterComponent extends AppBaseComponent implements OnInit {
 	public formGroup: FormGroup;
 	passwordStateMatcher = new PasswordStateMatcher();
+
+	private readonly authService: AuthService = inject(AuthService);
+	private readonly router: Router = inject(Router);
 
 	constructor(private fb: FormBuilder) {
 		super();
@@ -34,5 +40,20 @@ export class RegisterComponent extends AppBaseComponent implements OnInit {
 		);
 	}
 
-	public async register(): Promise<void> {}
+	public async register(): Promise<void> {
+		if (this.formGroup.valid) {
+			let request: AuthRequestSignUpDto;
+			request = {
+				username: this.formGroup.controls['username'].value,
+				password: this.formGroup.controls['password'].value,
+				authRolesRequestDto: {
+					rolesListName: ['INVITED', 'USER']
+				}
+			};
+			await lastValueFrom(this.authService.signUp(request)).then((response) => {
+				alert("Your registration was sucessfully completed...")
+				this.router.navigate(['/']);
+			});
+		}
+	}
 }
